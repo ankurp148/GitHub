51547dbed33e147fe0ebb99ea19cb670c155b58b
ankur <ankurp1481995@gmail.com>
2024-03-07 12:24:08 +0530

    5 commit

--- src/test/java/Practice/java.java
+++ src/test/java/Practice/java.java
@@ -5,6 +5,7 @@ public class java {
 	public static void main(String[] args) {
 		System.out.println("masala dosa");
 		System.out.println("paneer dosa");
+		System.out.println("mashroom biryani");
 
 	}
 
